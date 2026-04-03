7714c0fb63276f27a34f491197b4cbf82310d47a
kprabhneet <kprabhneet0@gmail.com>
2026-04-03 09:38:36 +0530

    first commit

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get('DEBUG','False') == 'True'
-ALLOWED_HOSTS = enviro.get("ALLOWED_HOSTS").split("")
+ALLOWED_HOSTS = environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
